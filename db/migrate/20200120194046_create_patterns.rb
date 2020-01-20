2ace632757107fdc2b7c2f445dcc451be30c9b1a
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2020-01-20 17:11:38 -0300

    Created initial models

---
+++ db/migrate/20200120194046_create_patterns.rb
@@ -0,0 +1,20 @@
+class CreatePatterns < ActiveRecord::Migration[5.2]
+  def change
+    create_table :patterns do |t|
+      t.string :url
+      t.string :string
+      t.string :color
+      t.string :string
+      t.string :link
+      t.string :string
+      t.string :board
+      t.string :string
+      t.string :width
+      t.string :integer
+      t.string :height
+      t.string :integer
+
+      t.timestamps
+    end
+  end
+end
